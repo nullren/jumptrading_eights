b75aafa7eff71585a5e809b1b8c67466e520ed5d
nullren <chopflop@gmail.com>
2012-10-30 14:56:15 -0700

    4th submission - added SSE code
    Congratulations, it worked! It ran in 0.476345 seconds with < 0.5%
    error. Can you optimize your code and improve on that?

--- compute.h
+++ compute.h
@@ -1,6 +1,7 @@
 #include "Data.h"
 #include <stdio.h>
 #include <cmath>
+#include <xmmintrin.h>
 
 float compute(const Data& d);
 
@@ -8,10 +9,15 @@ float ok_power(const float);
 float (*power)(const float) = ok_power;
 
 float compute_hmean(const float *);
+float sse_compute_hmean(const float *);
+float (*hmean)(const float *) = sse_compute_hmean;
 
 void kahan_action(const float, float *, float *);
 float kahan_sum(const float *, const int, const int);
 float (*sum)(const float *, const int, const int) = kahan_sum;
+
+__m128 sse_sqr(const __m128);
+__m128 sse_power(const __m128);
 
 float compute(const Data& d)
 {
@@ -23,7 +29,7 @@ float compute(const Data& d)
   for(int i = 0; i < N; ++i)
   {
     const float *v = d.value(i);
-    float m = compute_hmean(v);
+    float m = hmean(v);
 
     kahan_action(m, &avg, &c);
   }
@@ -65,6 +71,39 @@ float compute_hmean(const float *x)
   // raise power and invert
   for(int i=0; i<8; ++i)
     y[i] = power(x[i]);
+
+  float s = sum(y, 0, 8);
+  float r = 8/s;
+
+  if( std::isnan(r) )
+    r = 0;
+
+  return r;
+}
+
+__m128 sse_sqr(const __m128 d)
+{
+  return _mm_mul_ps(d,d);
+}
+
+__m128 sse_power(const __m128 d)
+{
+  __m128 f = _mm_rsqrt_ps(_mm_rsqrt_ps(_mm_rsqrt_ps(d)));
+  __m128 g = sse_sqr(sse_sqr(sse_sqr(d)));
+  return _mm_div_ps(f,g);
+}
+
+float sse_compute_hmean(const float *x)
+{
+  float y[8];
+
+  __m128 *sx = (__m128*)x;
+  __m128 *sy = (__m128*)y;
+
+  // raise power and invert
+  for(int i=0; i<2; ++i){
+    sy[i] = sse_power(sx[i]);
+  }
 
   float s = sum(y, 0, 8);
   float r = 8/s;
