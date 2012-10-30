650360490478af305a7718eec1421f048bf37973
nullren <chopflop@gmail.com>
2012-10-30 16:27:00 -0700

    shortened code, took out extra stuff

--- compute.h
+++ compute.h
@@ -5,16 +5,10 @@
 
 float compute(const Data& d);
 
-float ok_power(const float);
-float (*power)(const float) = ok_power;
-
-float compute_hmean(const float *);
 float sse_compute_hmean(const float *);
 float (*hmean)(const float *) = sse_compute_hmean;
 
 void kahan_action(const float, float *, float *);
-float kahan_sum(const float *, const int, const int);
-float (*sum)(const float *, const int, const int) = kahan_sum;
 
 __m128 sse_sqr(const __m128);
 __m128 sse_power(const __m128);
@@ -45,41 +39,6 @@ void kahan_action(const float x, float *s, float *c)
 
   *c = (t - *s) - y;
   *s = t;
-}
-
-float kahan_sum(const float *x, const int i, const int n)
-{
-  float s = x[i];
-  float c = 0;
-
-  for(int j = i+1; j < n; ++j)
-    kahan_action(x[j], &s, &c);
-
-  return s;
-}
-
-float ok_power(const float d)
-{
-  float f = sqrtf(sqrtf(sqrtf(d)));
-  float g = pow(pow(pow(d,2),2),2);
-  return 1/(f*g);
-}
-
-float compute_hmean(const float *x)
-{
-  float y[8];
-
-  // raise power and invert
-  for(int i=0; i<8; ++i)
-    y[i] = power(x[i]);
-
-  float s = sum(y, 0, 8);
-  float r = 8/s;
-
-  if( std::isnan(r) )
-    r = 0;
-
-  return r;
 }
 
 __m128 sse_sqr(const __m128 d)
